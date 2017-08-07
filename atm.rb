9442ea1d78536de9815ff641f434cee6885f4cbf
Ric Lavers <riclavers@Rics-MacBook-Pro-2.local>
2017-08-07 16:23:38 +1000

    complete w/ only one class

--- atm.rb
+++ atm.rb
@@ -8,18 +8,25 @@ https:/stackoverflow.com/questions/2068027/grabbing-snapshots-from-webcams-in-ru
 https://stackoverflow.com/questions/2068027/grabbing-snapshots-from-webcams-in-ruby/39938139#39938139
 
  MVP = minimal viable product
+
+
 =end
 
+system("clear") #clears the screen
 
 class Atm
   require 'io/console'
-
-  def initialize()
-    @balance= 1000
+  require 'colorize'
+  def initialize(balance)
+    @balance= balance
+    @number = 0
   end
+
+  attr_accessor :balance, :number
 
   def greeting
     #checks the pin for AAB customers, charges $2 for not AAB
+    slogan ="We'll Bank with Almost Anyone".green
     def aab_cus
       puts "It looks like your not a AAB customer \n a $2 charge will be incurred"
       puts "Would you like to proceed [y/n]"
@@ -34,10 +41,11 @@ class Atm
         aab_cus
       end
     end
+    puts
     puts "Hello, welcome to Almost Anything Banking [AAB]"
-    puts "             __"
-    puts "   /\\   /\\  | _)"
-    puts "  / -\\ / -\\ |__)"
+    puts "             __".yellow
+    puts "   /\\   /\\  | _)".yellow
+    puts "  / -\\ / -\\ |__)".yellow+"    #{slogan}"
     puts
     puts "please enter your Personal Identification Number [PIN]"
     text = STDIN.noecho(&:gets)
@@ -74,10 +82,58 @@ class Atm
       check_balance
     end
   end
+#generates one of three adverts
+  def advert
+    #number = rand(3)
+    @number +=1
+    if @number == 3
+      @number = 0
+    end
+    #puts "#{number}".red
+    case @number
+    when 0
+      highlight = "till death home loans".red
+      l1="         Can't afford a house?".blue
+      l2="check out AAB's ".blue + "#{highlight} "+ "today".blue
+      l3="           because at AAB...".blue
+      l4="      We'll Bank with Almost Anyone".green
+      return l1,l2,l3,l4
+      #print l2
+      #print l3
+      #print l4
+    when 1
+      highlight = "".red
+      l1="80% of all Hipsters can't afford the new MacBook Pro,  ".blue
+      l2="     forcing them to live without touchbars".blue
+      l3="           Sponsor a Hipsters today ".green
+      l4="   and ensure tomorrows tech is used today".green
+      return l1,l2,l3,l4
+      #print l2
+      #print l3
+      #print l4
+    when 2
+      highlight = "".red
+      l1="Travel on us, with the new AAB credit card   ".blue
+      l2="     earn 1 point per $1 spent*".blue
+      l3="          ".green
+      l4="*fights cost on average 1 million points".light_black
+      return l1,l2,l3,l4
+      #print l2
+      #print l3
+      #print l4
+    end
+  end
 #displays the Menu, linking user to other methods
   def menu
-    puts "What would you like to do"
-    puts "\n 1- Check balance \n 2- Withdraw cash \n 3- Deposit cash\n 4- Exit"
+
+    ad=advert
+    puts
+    puts "What would you like to do".yellow
+    puts "
+    1- Check balance        "+ad[0]+"
+    2- Withdraw cash        "+ad[1]+"
+    3- Deposit cash         "+ad[2]+"
+    4- Exit                 "+ad[3]
     choice = gets.chomp.to_i
     case choice
       when 1
@@ -106,9 +162,9 @@ class Atm
            l1=""
            l2=""
            l3=""
-           num.times{|i|l1=l1 + " ______ "}
-           num.times{|i|l2=l2 + "|  $50 |"}
-           num.times{|i|l3=l3 + "|______|"}
+           num.times{|i|l1=l1 + " ______ ".green}
+           num.times{|i|l2=l2 + "|  $50 |".green}
+           num.times{|i|l3=l3 + "|______|".green}
            puts l1
            puts l2
            puts l3
@@ -117,9 +173,9 @@ class Atm
            l1=""
            l2=""
            l3=""
-           num.times{|i|l1=l1 + " ______ "}
-           num.times{|i|l2=l2 + "|  $20 |"}
-           num.times{|i|l3=l3 + "|______|"}
+           num.times{|i|l1=l1 + " ______ ".green}
+           num.times{|i|l2=l2 + "|  $20 |".green}
+           num.times{|i|l3=l3 + "|______|".green}
            puts l1
            puts l2
            puts l3
@@ -151,9 +207,16 @@ class Atm
       sleep(0.3)
       print "$ "
       amount=gets.chomp.to_i
+
       if amount == 0
         sleep(0.3)
         puts "Sorry please enter an number amount"
+      elsif amount > @balance
+          puts "Sorry, there is only $ #{@balance} in your account"
+          puts
+          amount = 0
+          withdraw
+          break
       else
         if all_combos.include?(amount)
           puts "depositing"
@@ -164,6 +227,8 @@ class Atm
           puts
           fifty(combo_calc(amount)[0])
           twenty(combo_calc(amount)[1])
+          @balance = @balance - amount
+          check_balance
           go = true
         elsif amount >500
           puts "sorry, maximum withdrawal is $500"
@@ -175,8 +240,7 @@ class Atm
     end
     #maybe enter a money graphic :)
 
-    @balance = @balance - amount
-    check_balance
+
     #puts "your balance is : $#{sprintf('%.2f', balance)}"
     #needs to ask for receipt and another option.
 
@@ -207,10 +271,10 @@ class Atm
   end
 #simple goodbye message, called when cus exits program
   def goodbye
-    puts "Goodbye, thankyou for banking with AAB"
+    puts "Goodbye, thankyou for banking with AAB".yellow
   end
-
 end
+
 #calls script.
-atm = Atm.new()
-atm.greeting()
+myAtm = Atm.new(500)
+myAtm.greeting()
